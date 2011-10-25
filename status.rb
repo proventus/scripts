d2d8d750ef2a0ef08881a8ba330db101dce4230e
Alexander Kramnik <akramnik@gmail.com>
2011-10-24 19:16:17 -0500

    initial commit

---
+++ status.rb
@@ -0,0 +1,30 @@
+require 'net/http'
+require 'net/smtp'
+
+def check_http host, path
+  http = Net::HTTP.new(host)
+  http.open_timeout = 20
+  http.read_timeout = 20
+  resp, data = http.get path, nil
+#  unless resp.code == "200"
+    send_mail "Received code #{resp.code} from #{host}"
+#  end
+end
+
+def send_mail subject
+  from = "support@proventustechnologies.com"
+  to = "akramnik@proventustechnologies.com"
+
+  msg = <<END_OF_MESSAGE
+From: #{from}
+To: #{to}
+Subject: #{subject}
+END_OF_MESSAGE
+  Net::SMTP.start('proventustechnologies.com') do |smtp|
+    smtp.send_message msg, from, to
+  end
+end
+
+check_http 'account.proventustechnologies.com', '/portal/'
+check_http 'lb1.proventustechnologies.com', '/api/json'
+check_http 'lb2.proventustechnologies.com', '/api/json'
